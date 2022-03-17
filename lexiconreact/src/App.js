bfcc0440854322e0dcf80c4a6604970d066feb23
Vingnir <vingnir@live.se>
2022-03-17 16:15:09 +0100

    adding navbar

--- lexiconreact/src/App.js
+++ lexiconreact/src/App.js
@@ -1,27 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Sidebar from './sidebar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          No mas cerveza gordito 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Time to sober up
-        </a>
-        <Button>Test Button</Button>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Sidebar />
+      </div>
+    </Router>
   );
 }
 
